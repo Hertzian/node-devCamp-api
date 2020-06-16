a42017466eb471c9fae1cff034f3c818f25a2d12
Lalitus <tallitosan@gmail.com>
2020-06-16 14:42:09 -0500

    tweaked middleware error with duplicate key validation and validation errors

--- middleware/error.js
+++ middleware/error.js
@@ -4,16 +4,29 @@ const errorHandler = (err, req, res, next) => {
     let error = {...err};
 
     error.message = err.message;
+
     // Log to console for dev
-    console.log(err.stack.red);
+    // console.log(error);
+    console.log(err);
 
     // Mongoose bad ObjectId
     if(err.name === 'CastError'){
         const message = `Resource not found id of ${err.value}`;
         error = new ErrorResponse(message, 404);
     }
-    console.log(err.name)
 
+    // Mongoose duplicate key
+    if(err.code === 11000){
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation errors
+    if(err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
+    }
+    
     res.status(error.statusCode || 500).json({
         success: false,
         error: error.message || 'Server Error'
